88867a7de7624aba7bf6302cdbdfb71aa30419a4
Clément Vollet <cvollet@gmail.com>
2008-02-13 03:26:33 +0000

    Updates : add the fcp package of jSite
    git-svn-id: file:///home/toad/git-migration/temprepository/trunk/apps/thingamablog@17860 67a373e5-eb02-0410-a15c-ee090a768436

---
+++ src/net/sf/thingamablog/util/freenet/fcp/Persistence.java
@@ -0,0 +1,87 @@
+/*
+ * todesbaum-lib - 
+ * Copyright (C) 2006 David Roden
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ */
+
+package net.sf.thingamablog.util.freenet.fcp;
+
+/**
+ * The possible persistence options. This specify whether (and for how long) the
+ * node remembers to execute a request and the results. Possible values are
+ * <code>connection</code>, <code>reboot</code>, and <code>forever</code>.
+ * <code>connection</code> means that a request is aborted as soon as the
+ * connection to the node is severed. <code>reboot</code> means that a request
+ * is remembered as long as the node is running but not after restarts.
+ * <code>forever</code> finally means that a request persists until it is
+ * explicitely deleted.
+ * 
+ * @author David Roden &lt;droden@gmail.com&gt;
+ * @version $Id: Persistence.java 9647 2006-07-17 18:24:50Z bombe $
+ * @see de.todesbaum.util.freenet.fcp2.ModifyPersistentRequest
+ * @see de.todesbaum.util.freenet.fcp2.RemovePersistentRequest
+ */
+public final class Persistence {
+
+	/**
+	 * Denotes that a request should be terminated if the connection to the node
+	 * is severed.
+	 */
+	public static final Persistence CONNECTION = new Persistence("connection");
+
+	/** Denotes that a request should be remembered until the node is restarted. */
+	public static final Persistence REBOOT = new Persistence("reboot");
+
+	/**
+	 * Denotes that a request should be remembered until it is explicitely
+	 * deleted.
+	 */
+	public static final Persistence FOREVER = new Persistence("forever");
+
+	/** The name of this persistence option. */
+	private String name;
+
+	/**
+	 * Private constructor that creates a persistence option with the specified
+	 * name.
+	 * 
+	 * @param name
+	 *            The name of the persistence option.
+	 */
+	private Persistence(String name) {
+		this.name = name;
+	}
+
+	/**
+	 * Returns the name of this persistence option.
+	 * 
+	 * @return The name of this persistence option
+	 */
+	public String getName() {
+		return name;
+	}
+
+	/**
+	 * Returns a textual representation of this persistence option. The result
+	 * is identical to calling {@link #getName()}.
+	 * 
+	 * @return The name of this persistence option
+	 */
+	public String toString() {
+		return name;
+	}
+
+}
---
+++ src/net/sf/thingamablog/util/freenet/fcp/PriorityClass.java
@@ -0,0 +1,93 @@
+/*
+ * todesbaum-lib - 
+ * Copyright (C) 2006 David Roden
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ */
+
+package net.sf.thingamablog.util.freenet.fcp;
+
+/**
+ * The possible priority classes. Possible values are, in order of descending
+ * priority: <code>maximum</code> (anything more important than fproxy),
+ * <code>interactive</code> (fproxy), <code>semi-interactive</code> (fproxy
+ * immediate mode large file downloads, not to disk), <code>updatable</code>
+ * (updatable site checks), <code>bulk</code> (large file downloads to disk),
+ * <code>prefetch</code>, <code>minimum</code>.
+ * 
+ * @author David Roden &lt;droden@gmail.com&gt;
+ * @version $Id: PriorityClass.java 9647 2006-07-17 18:24:50Z bombe $
+ */
+public final class PriorityClass {
+
+	/** Denotes <code>maximum</code> priority class. */
+	public static final PriorityClass MAXIMUM = new PriorityClass("maximum", 0);
+
+	/** Denotes <code>interactive</code> priority class. */
+	public static final PriorityClass INTERACTIVE = new PriorityClass("interactive", 1);
+
+	/** Denotes <code>semi-interactive</code> priority class. */
+	public static final PriorityClass SEMI_INTERACTIVE = new PriorityClass("semiInteractive", 2);
+
+	/** Denotes <code>updatable</code> priority class. */
+	public static final PriorityClass UPDATABLE = new PriorityClass("updatable", 3);
+
+	/** Denotes <code>bulk</code> priority class. */
+	public static final PriorityClass BULK = new PriorityClass("bulk", 4);
+
+	/** Denotes <code>prefetch</code> priority class. */
+	public static final PriorityClass PREFETCH = new PriorityClass("prefetch", 5);
+
+	/** Denotes <code>minimum</code> priority class. */
+	public static final PriorityClass MINIMUM = new PriorityClass("minimum", 6);
+
+	/** The name of the priority class. */
+	private String name;
+
+	/** The value of the priority class. */
+	private int value;
+
+	/**
+	 * Creates a new priority class with the specified name and value.
+	 * 
+	 * @param name
+	 *            The name of the priority class
+	 * @param value
+	 *            The value of the priority class
+	 */
+	private PriorityClass(String name, int value) {
+		this.name = name;
+		this.value = value;
+	}
+
+	/**
+	 * Returns the name of this priority class.
+	 * 
+	 * @return The name of this priority class
+	 */
+	public String getName() {
+		return name;
+	}
+
+	/**
+	 * Returns the value of this priority class.
+	 * 
+	 * @return The value of this priority class
+	 */
+	public int getValue() {
+		return value;
+	}
+
+}
